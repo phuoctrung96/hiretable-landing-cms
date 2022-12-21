43b90998c4f1d50f0298fed94ede1c9b3a65a680
It-Superdev <105212989+It-SuperDev@users.noreply.github.com>
2022-12-21 14:22:37 +0600

    [update] send mail

--- src/api/custom/controllers/custom.js
+++ src/api/custom/controllers/custom.js
@@ -4,29 +4,42 @@
  * A set of functions called "actions" for `custom`
  */
 
+const sndMail = async (data) => {
+  try {
+    await strapi.plugins['email'].services.email.send({
+      to: data.email,
+      from: 'admin@strapi.io',
+      subject: 'Your Deal',
+      text: `
+        PRICE: ${data.price}
+        LOCATION: ${data.location}
+        First Name: ${data.firstName}
+        Last Name: ${data.lastName}
+        Phone: ${data.phone}
+        Site URL: ${data.url}
+      `,
+    });
+  } catch (error) {
+    console.log('maile error: ', error.message)
+  }
+}
+
 module.exports = {
   exampleAction: async (ctx, next) => {
     try {
-      let data = ctx.request.body.data;
+      let data = ctx.request.body;
       if (Object.keys(data).length) {
-        const rdata = await strapi.entityService.create('api::deal.deal', {
-          data
+        const entry = await strapi.db.query('api::deal.deal').findOne({
+          where: { email: data.email },
         });
 
-        await strapi.plugins['email'].services.email.send({
-          to: data.email,
-          from: 'admin@strapi.io',
-          subject: 'Your Deal',
-          text: `
-            PRICE: ${data.price}
-            LOCATION: ${data.location}
-            First Name: ${data.firstName}
-            Last Name: ${data.lastName}
-            Phone: ${data.phone}
-            Site URL: ${data.url}
-          `,
-        });
-        return rdata
+        if (entry) {
+          return { status: false, message: 'Email already exists.' }
+        }
+
+        const rdata = await strapi.entityService.create('api::deal.deal', { data });
+        sndMail(data);
+        return rdata;
       } else {
         return { status: false, message: 'invalid data' }
       }
