2c9ccb6e6708c7f4bb3c0c096eea375926ced416
phuoc-dev <phuoc.trung.nguyen.96@gmail.com>
2022-12-22 22:11:24 +0700

    Removed the comments

--- config/database.js
+++ config/database.js
@@ -11,14 +11,3 @@ module.exports = ({ env }) => ({
     useNullAsDefault: true,
   }
 });
-
-// const path = require('path');
-// module.exports = ({ env }) => ({
-//   connection: {
-//     client: 'sqlite',
-//     connection: {
-//       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-//     },
-//     useNullAsDefault: true,
-//   },
-// });
