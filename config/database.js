6525386fd1ead7ecfc07e5298e44cd4c94d6ef7d
It-Superdev <it.dev0116@gmail.com>
2022-12-22 20:20:17 +0600

    [update] change DB from sqlite to mySQL

--- config/database.js
+++ config/database.js
@@ -1,11 +1,24 @@
-const path = require('path');
-
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: "mysql",
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env("DATABASE_HOST", "localhost"),
+      port: env("DATABASE_PORT", 3306),
+      database: env("DATABASE_NAME", "strapi"),
+      user: env("DATABASE_USERNAME", "root"),
+      password: env("DATABASE_PASSWORD", ""),
     },
     useNullAsDefault: true,
-  },
+  }
 });
+
+// const path = require('path');
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'sqlite',
+//     connection: {
+//       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+//     },
+//     useNullAsDefault: true,
+//   },
+// });
